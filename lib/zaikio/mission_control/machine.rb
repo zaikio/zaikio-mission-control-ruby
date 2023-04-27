cc99508aeb78eca8a665ecf9c75f066ce0d87fab
Jalyna <js@zaikio.com>
2023-04-27 08:54:19 +0200

    Remove availabilities again (#7)
    since it will be part of the hub client instead

--- lib/zaikio/mission_control/machine.rb
+++
@@ -1,9 +0,0 @@
-module Zaikio
-  module MissionControl
-    class Machine < Base
-      # Associations
-      has_many :availabilities, class_name: "Zaikio::MissionControl::Availability",
-                                uri: "machines/:machine_id/availabilities"
-    end
-  end
-end
